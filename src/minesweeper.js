b737b3d964ea6d67d6a6dc9cea8d237105a1260c
Kevin Ketterer <kketterer@dart.org>
2018-12-17 14:58:29 -0600

    Dynamically Generate Game Boards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -11,5 +11,36 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     return board;
 }
 
-let newBoard = generatePlayerBoard(3, 5);
-console.log(newBoard);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+
+    for (let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        /*An important note: The code in your while loop has the potential to place bombs on top of already existing bombs.
+        This will be fixed when you learn about control flow.*/
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfRows);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+
+    return board;
+}
+
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+printBoard(bombBoard);
